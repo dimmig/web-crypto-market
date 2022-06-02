416f4efd858df36450215b70e97f3dc022ea1421
Dima Galagan <galagan.dm@gmail.com>
2022-06-02 15:27:34 +0300

    added raw contract-side

--- contract/dev_deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release  \
-        && near dev-deploy target/wasm32-unknown-unknown/release/archimarket.wasm  \
